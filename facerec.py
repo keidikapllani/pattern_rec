4b69e93e6167e233e6bbd75d6b033ac1303c579d
Keidi <kk2314@ic.ac.uk>
2018-11-14 20:02:14 +0000

    PCA not working

--- facerec.py
+++ facerec.py
@@ -51,39 +51,37 @@ def load_data():
 
 def project(W, X, mu=None):
 	if mu is None:
-		return np.dot(X,W)
-	return np.dot(X - mu, W)
+		return np.dot(X.T,W)
+	return np.dot((X - mu).T, W)
 
 def reconstruct(W, Y, mu=None):
-	if mu is None:
-		return np.dot(Y,W.T)
-	return np.dot(Y, W.T) + mu
+    if mu is None:
+        return np.dot(Y,W.T)
+    return np.dot(Y, W.T) + mu
 
 def pca(X, y, num_components=0):
-	[n,d] = X.shape
-	if (num_components <= 0) or (num_components>n):
-		num_components = n
-	mu = X.mean(axis=0)
-	X = X - mu
-	if n>d:
-		C = np.dot(X.T,X)
-		[eigenvalues,eigenvectors] = np.linalg.eigh(C)
-	else:
-		C = np.dot(X,X.T)
-		[eigenvalues,eigenvectors] = np.linalg.eigh(C)
-		eigenvectors = np.dot(X.T,eigenvectors)
-		for i in range(n):
-			eigenvectors[:,i] = eigenvectors[:,i]/np.linalg.norm(eigenvectors[:,i])
+    [d, n] = X.shape
+    if (num_components <= 0) or (num_components>n):
+        num_components = n
+    mu = X.mean(axis=1).reshape((d,1))
+    
+    X = X - mu
+   
+    C = np.dot(X.T,X)
+    [eigenvalues,eigenvectors] = np.linalg.eigh(C)
+    eigenvectors = np.dot(X,eigenvectors)
+    for i in range(n):
+        eigenvectors[:,i] = eigenvectors[:,i]/np.linalg.norm(eigenvectors[:,i])
 	# or simply perform an economy size decomposition
 	# eigenvectors, eigenvalues, variance = np.linalg.svd(X.T, full_matrices=False)
 	# sort eigenvectors descending by their eigenvalue
-	idx = np.argsort(-eigenvalues)
-	eigenvalues = eigenvalues[idx]
-	eigenvectors = eigenvectors[:,idx]
+    idx = np.argsort(-eigenvalues)
+    eigenvalues = eigenvalues[idx]
+    eigenvectors = eigenvectors[:,idx]
 	# select only num_components
-	eigenvalues = eigenvalues[0:num_components].copy()
-	eigenvectors = eigenvectors[:,0:num_components].copy()
-	return [eigenvalues, eigenvectors, mu]
+    eigenvalues = eigenvalues[0:num_components].copy()
+    eigenvectors = eigenvectors[:,0:num_components].copy()
+    return [eigenvalues, eigenvectors, mu]
 		
 def lda(X, y, num_components=0):
 	y = np.asarray(y)
